23fc0961f4e3603b71057bfc825aa0e9ce7455a4
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-07-19 02:07:13 -0300

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-const path = require('path');
-
-module.exports = {
+const nextConfig = {
   webpack: (config) => {
     config.module.rules.push({
       test: /\.css$/,
       use: ['style-loader', 'css-loader'],
-      include: path.resolve(__dirname, 'node_modules/slick-carousel')
+      include: path.resolve(process.cwd(), 'node_modules/slick-carousel')
     });
     return config;
   }
 };
+
+export default nextConfig;
