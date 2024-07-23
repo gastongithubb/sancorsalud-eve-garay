064ad38a3658f1ee14fda6205b4f2ec7a650b94f
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-07-23 02:55:03 -0300

    .

--- app/balanceMensual/page.tsx
+++
@@ -1,9 +0,0 @@
-// import BalanceMensual from '../components/BalanceMensual/FileUpload';
-
-// export default function TeamPage() {
-//   return (
-//     <div>
-//       <BalanceMensual />
-//     </div>
-//   );
-// }
