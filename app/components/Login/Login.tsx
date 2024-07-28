5426b25e804ecbc37037327d70a9a6588ba6cc4c
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-07-28 17:13:17 -0300

    ssss

--- app/components/Login/Login.tsx
+++ app/components/Login/Login.tsx
@@ -87,7 +87,7 @@ export default function LoginPage() {
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don&apos;t have an account?{' '}
           <a href="#" className="font-medium text-blue-600 hover:underline">
             Sign up
           </a>
