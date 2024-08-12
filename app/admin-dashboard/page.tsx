33b95cbefa6f721e2bada3a163471374f09d8abd
Gaston Alvarez <gastonalvarez604@gmail.com>
2024-08-12 10:20:06 -0300

    se cambian nombres

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,26 +1,26 @@
 import React, { useState } from 'react';
-import AdminDashboard from '../components/Admin-Dashboard/admin';
+import AdminDashboard from '../components/Admin-Dashboard/mensual';
 import MetricasTrimestralDashboard from '../components/Admin-Dashboard/trimestral-admin';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/app/components/ui/tabs"
 
 const DashboardOption = {
-  ADMIN: 'admin',
+  MENSUAL: 'balance-mensual',
   METRICAS_TRIMESTRALES: 'metricas-trimestrales',
 };
 
 export default function TeamPage() {
-  const [selectedDashboard, setSelectedDashboard] = useState(DashboardOption.ADMIN);
+  const [selectedDashboard, setSelectedDashboard] = useState(DashboardOption.MENSUAL);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Team Dashboard</h1>
       
-      <Tabs defaultValue={DashboardOption.ADMIN} onValueChange={(value) => setSelectedDashboard(value)}>
+      <Tabs defaultValue={DashboardOption.MENSUAL} onValueChange={(value) => setSelectedDashboard(value)}>
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value={DashboardOption.ADMIN}>Admin Dashboard</TabsTrigger>
+          <TabsTrigger value={DashboardOption.MENSUAL}>Balance Mensual</TabsTrigger>
           <TabsTrigger value={DashboardOption.METRICAS_TRIMESTRALES}>Métricas Trimestrales</TabsTrigger>
         </TabsList>
-        <TabsContent value={DashboardOption.ADMIN}>
+        <TabsContent value={DashboardOption.MENSUAL}>
           <AdminDashboard />
         </TabsContent>
         <TabsContent value={DashboardOption.METRICAS_TRIMESTRALES}>
